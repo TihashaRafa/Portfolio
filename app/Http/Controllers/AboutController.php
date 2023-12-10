43e4baeb8ea6c9dca9a6b086295caa3613a88622
Tihasha16 <rafatisha.98@gmail.com>
2023-12-10 17:06:40 +0600

    about page edit successfully

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -51,28 +51,27 @@ class AboutController extends Controller
 
     public function update(Request $request, $id)
     {
-        $validatedData = $request->validate([
-            
-        ]);
 
         $about = about::find($id);
-        $about->description_one                 = $validatedData['description_one'];
-        $about->description_two                 = $validatedData['description_two'];
-        $about->description_three               = $validatedData['description_three'];
-        $about->age                             = $validatedData['age'];
-        $about->birth                           = $validatedData['birth'];
-        $about->experience_year                 = $validatedData['experience_year'];
-        $about->experience                      = $validatedData['experience'];
-        $about->project_num                     = $validatedData['project_num'];
-        $about->project                         = $validatedData['project'];
-        $about->client_num                      = $validatedData['client_num'];
-        $about->client                          = $validatedData['client'];
+        $about->description_one                 = $request['description_one'];
+        $about->description_two                 = $request['description_two'];
+        $about->description_three               = $request['description_three'];
+        $about->age                             = $request['age'];
+        $about->birth                           = $request['birth'];
+        $about->experience_year                 = $request['experience_year'];
+        $about->experience                      = $request['experience'];
+        $about->project_num                     = $request['project_num'];
+        $about->project                         = $request['project'];
+        $about->client_num                      = $request['client_num'];
+        $about->client                          = $request['client'];
        
        
         $about->save();
 
         return redirect(route('about.index'))->with('success', 'Data updated successfully.');
     }
+
+
 
     public function destroy($id){
         about::destroy($id);
